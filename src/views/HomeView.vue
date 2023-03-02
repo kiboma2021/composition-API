c58b13d5e0666c4ecc7d67f7264c00f27bf1f533
kiboma <omayioben@gmail.com>
2023-03-02 18:29:15 +0300

    Hide and show posts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <div v-if="showPosts">
+      <PostList :posts="posts"/>
+    </div>
 
-    <PostList :posts="posts"/>
+    <button @click="showPosts = !showPosts">Show posts</button>
   </div>
 </template>
 
@@ -18,7 +21,10 @@ export default {
       {title: 'Welcome to the blog', body: 'Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media.', id: 1},
       {title: 'Top 5 Python and Django Tips', body: 'Django is a high-level Python web framework that enables rapid development of secure and maintainable websites. Built by experienced developers, Django takes care of much of the hassle of web development, so you can focus on writing your app without needing to reinvent the wheel. It is free and open source, has a thriving and active community, great documentation, and many options for free and paid-for support.', id: 2}
     ])
-    return { posts }
+
+    const showPosts = ref(true)
+
+    return { posts, showPosts}
   }
 
 }
