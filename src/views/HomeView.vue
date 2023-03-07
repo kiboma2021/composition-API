ab4d87741df76ee0c4ce9748ead1885640733ded
kiboma <omayioben@gmail.com>
2023-03-07 08:35:37 +0300

    Get data from sync api and catch an error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <div v-if="showPosts">
+    <div v-if="error"> {{ error }}</div>
+    <div v-else>
+      <div v-if="showPosts">
       <PostList :posts="posts"/>
     </div>
-
     <button @click="showPosts = !showPosts">Show posts</button>
     <button @click="posts.pop()">Delete Post</button>
+    </div>
   </div>
 </template>
 
@@ -18,14 +20,28 @@ export default {
   name: 'HomeView',
   components: {PostList},
   setup(){
-    const posts = ref([
-      {title: 'Welcome to the blog', body: 'Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media.', id: 1},
-      {title: 'Top 5 Python and Django Tips', body: 'Django is a high-level Python web framework that enables rapid development of secure and maintainable websites. Built by experienced developers, Django takes care of much of the hassle of web development, so you can focus on writing your app without needing to reinvent the wheel. It is free and open source, has a thriving and active community, great documentation, and many options for free and paid-for support.', id: 2}
-    ])
-
+    const posts = ref([])
+    const error = ref(null)
     const showPosts = ref(true)
 
-    return { posts, showPosts}
+    const load = async () => {
+      try {
+        let data = await fetch ('http://localhost:3000/posts')
+        if (!data.ok){
+          throw Error("No data available")
+        }
+        posts.value = await data.json()
+      }
+      catch(err){
+        error.value = err.message
+        console.log(error.value)
+      }
+
+    }
+
+    load()
+
+    return { posts, showPosts, error}
   }
 
 }
