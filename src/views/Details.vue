6d0ee0f2ff3e460a0aeabc4cd1f5cc874c5ceabe
kiboma <omayioben@gmail.com>
2023-03-09 08:11:24 +0300

    Update details view

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,12 +1,19 @@
 <template>
-  <p>Details</p>
+  <div v-if="error">{{ error }}</div>
+  <div v-if="post">
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.body }}</p>
+  </div>
 </template>
 
 <script>
+import getPost from '@/compositors/getPost'
 export default {
   props: ['id'],
   setup(props) {
-    
+    const { post, error, load } = getPost(props.id)
+    load()
+    return { post, error };
   }
 
 }
