d409693f507c2bfbad87629aad1362ee00a2cd03
kiboma <omayioben@gmail.com>
2023-03-09 08:11:05 +0300

    Update details API

--- src/compositors/getPost.js
+++ src/compositors/getPost.js
@@ -1,13 +1,13 @@
 import { ref } from 'vue'
 
-const getPost = () => {
+const getPost = (id) => {
     const post = ref(null)
     const error = ref(null)
     const load = async () => {
         try{
-            let data = await fetch('http://localhost:3000/posts');
+            let data = await fetch('http://localhost:3000/posts/' + id);
             if(!data.ok){
-                throw Error("No data is fetched")
+                throw Error("Post not available")
             }
             post.value = await data.json()
         } catch (err) {
